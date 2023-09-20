dc60a037f0517eaef6c6bb83ace93387a844d2e3
tiendatAI <tiendatlework@gmail.com>
2023-09-20 11:51:24 +0700

    add more configs

--- nvim/lua/custom/init.lua
+++ nvim/lua/custom/init.lua
@@ -12,6 +12,9 @@ vim.o.scrolloff = 8
 vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
 
+-- enable relative number
+vim.wo.relativenumber = true 
+
 
 
 
