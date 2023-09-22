eaec249047cc8c39aba54474835215042e86e008
tiendatAI <tiendatlework@gmail.com>
2023-09-22 16:55:01 +0700

    add more configs

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -15,7 +15,6 @@ local plugins = {
     init = overrides.nvim_dap.init,
     config = overrides.nvim_dap.config,
   },
-
   -- dont make mistake
   {
     "jay-babu/mason-nvim-dap.nvim",
@@ -39,6 +38,7 @@ local plugins = {
     'Exafunction/codeium.vim',
     event = 'BufEnter'
   },
+
 
   -- python
   {
@@ -69,6 +69,7 @@ local plugins = {
     ft = overrides_rs.rust_lang.ft,
     init = overrides_rs.rust_lang.init,   
   },
+
 
   --atuo complete
   {
