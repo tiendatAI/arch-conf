d409456cfc09d9f30da1e0340d021131749b35ad
tiendatAI <tiendatlework@gmail.com>
2023-11-06 10:17:28 +0700

    new private

--- doom/config.el
+++ doom/config.el
@@ -99,3 +99,11 @@
 
 ;; Trigger completion immediately.
 (setq company-idle-delay 0)
+
+;; lsp config
+(with-eval-after-load 'lsp-mode
+  (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\.devenv\\'")
+  (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\.direnv\\'")
+  ;; or
+  ;; (add-to-list 'lsp-file-watch-ignored-files "[/\\\\]\\.my-files\\'")
+  )
--- doom/configs/org-ai.el
+++ doom/configs/org-ai.el
@@ -7,4 +7,4 @@
   (org-ai-global-mode) ; installs global keybindings on C-c M-a
   ) ; if you are using yasnippet and want `ai` snippets
 
-(setq org-ai-openai-api-token "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")
+(setq org-ai-openai-api-token "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")
