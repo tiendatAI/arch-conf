4377c01ddadb5b66b40663290f7fc9891f4b7b5a
tiendatAI <tiendatlework@gmail.com>
2023-11-30 11:04:52 +0700

    diagram for emacs

--- doom/configs/org.el
+++ doom/configs/org.el
@@ -1,10 +1,12 @@
 (setq org-directory "~/org/")
-(setq org-roam-directory "~/org/roam-notes/")
+(setq org-roam-directory "~/org/roam/")
 (setq org-agenda-files '( "~/org/TODO/every-day.org"
                           "~/org/TODO/long-term.org"
                           "~/org/TODO/tracking-habit.org" ))
 (setq org-noter-notes-search-path '("~/org/noter/"))
 (setq org-journal-dir "~/org/journal/")
+(setq ob-mermaid-cli-path "/nix/store/6jly5h71i8rgv2yx63c1qrc29g36nk3s-_at_mermaid-js_slash_mermaid-cli-10.2.4/bin/mmdc") ;; bad practice
+;; (setq ob-mermaid-cli-path (shell-command-to-string "which mmdc"))
 
 ;; record time I actually work
 (defun my-reset-org-clock ()
@@ -29,15 +31,12 @@
 ;; Set up for journal
 (setq org-journal-file-type 'yearly)
 
-(after! org
-  (setq org-todo-keywords ; This overwrites the default Doom org-todo-keywords
-        '(
-          (sequence
-           "TODO(t)"
-           "PROG(p)"
-           "BUGS(b)"
-           "DONE(d)"
-           )
-          )
-        )
-  )
+;; add mermaid to org-babel-load-languages
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((mermaid . t)
+   (scheme . t)
+   (rust . t)
+   (C . t)
+   (python . t)
+   ))
--- doom/packages.el
+++ doom/packages.el
@@ -4,8 +4,10 @@
 ;; customize theme
 (package! catppuccin-theme)
 
+
 ;; cursor
 (package! beacon)
+
 
 ;; better pdf tools
 (package! pdf-tools :recipe
@@ -20,42 +22,63 @@
 (package! image-roll :recipe
   (:host github
    :repo "dalanicolai/image-roll.el"))
+
 ;; remember position when leave pdf
 (package! saveplace-pdf-view)
+
 
 ;; better pomodoro
 (package! pomidor)
 
+
 ;; typst
 (package! typst-ts-mode :recipe (:type git :host sourcehut :repo "meow_king/typst-ts-mode"))
+
 
 ;;python autoload env
 (package! pyvenv)
 
+
 ;;rss for research paper
 (package! elfeed-score)
+
 
 ;;translate
 (package! google-translate)
 
+
 ;; display list of special comment (TODO, FIXME,  etc.)
 (package! comment-tags )
+
 
 ;; packages for org
 ;; notification on org
 (package! org-alert)
+
 ;; org ai for chatGPT
 (package! org-ai)
+
 ;; code rust in org mode
 (package! ob-rust)
+
 ;; download image from internet
 (package! org-download)
+
 ;; beter org-roam
 (package! org-roam-ui)
+
 ;;insert content of text with a link
-(package! org-transclusion) 
+(package! org-transclusion)
+
 ;; better agenda
 (package! org-super-agenda)
 (package! org-timeblock)
+
 ;; kaban board for project planning
 (package! org-kanban)
+
+;; kaban board for project planning
+(package! org-kanban)
+
+;; diagram in org-mode
+(package! ob-mermaid)
