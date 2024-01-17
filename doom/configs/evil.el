87b3251695da169155be5c011ffbafbac89aeef8
tiendatAI <tiendatlework@gmail.com>
2024-01-17 17:37:29 +0700

    new binding for emacs

--- doom/configs/evil.el
+++ doom/configs/evil.el
@@ -1,3 +1,15 @@
 ;;; configs/evil.el -*- lexical-binding: t; -*-
 
 (evil-global-set-key 'insert (kbd "C-l") 'evil-delete-backward-char)
+
+;; Match the word under cursor (i.e. make it an edit region). Consecutive presses will
+;; incrementally add the next unmatched match.
+(define-key evil-normal-state-map (kbd "M-m") 'evil-multiedit-match-and-next)
+;; Match selected region.
+(define-key evil-visual-state-map (kbd "M-m") 'evil-multiedit-match-and-next)
+;; Insert marker at point
+(define-key evil-insert-state-map (kbd "M-m") 'evil-multiedit-toggle-marker-here)
+
+;; Same as M-d but in reverse.
+(define-key evil-normal-state-map (kbd "M-M") 'evil-multiedit-match-and-prev)
+(define-key evil-visual-state-map (kbd "M-M") 'evil-multiedit-match-and-prev)
